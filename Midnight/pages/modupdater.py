bccd278974103542768174a99dabceb6c72f821e
Chip <itsjustchipoverhere@proton.me>
2025-06-27 18:08:34 +0700

    sidebar

--- Midnight/pages/modupdater.py
+++ Midnight/pages/modupdater.py
@@ -1,8 +1,14 @@
 import streamlit as st
+from modules.sidebar.sidebar import InitSidebar
+
+InitSidebar()
 
 from pathlib import Path
 from modules.mod_updater import ModUpdater
 from modules.deployments import DeployHistory
 
 # this shit gonna be hard
+
+
+
 
