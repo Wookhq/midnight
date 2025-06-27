9a076178b3f365c6af9da0bbeea4e7002db569bf
Chip <itsjustchipoverhere@proton.me>
2025-06-27 15:24:20 +0700

    init sidebar

--- Midnight/main.py
+++ Midnight/main.py
@@ -2,3 +2,10 @@
 Originally inspired by Kliko’s modding tool – https://github.com/thekliko/klikos-modding-tool
 """
 
+
+import streamlit as st
+from modules.sidebar.sidebar import InitSidebar
+
+InitSidebar()
+
+st.write("test")
