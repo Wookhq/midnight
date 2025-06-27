50a6df3da7aa9c78845ac6aff995214d2985ee2d
Chip <itsjustchipoverhere@proton.me>
2025-06-27 18:11:20 +0700

    add mod updater

--- Midnight/modules/sidebar/sidebar.py
+++ Midnight/modules/sidebar/sidebar.py
@@ -6,5 +6,6 @@ def InitSidebar():
     st.sidebar.caption("Version M-0.2.3")
 
     st.sidebar.page_link("main.py", label="Home")
+    st.sidebar.page_link("pages/modupdater.py", label="Mod updater")
     st.sidebar.page_link("pages/about.py", label="About")
 
