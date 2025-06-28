694a825690dd864dc3ccd08c7328fd17e48b2950
cihp <itsjustchipoverhere@proton.me>
2025-06-28 15:18:19 +0700

    fix sober cp

--- Midnight/pages/modupdater.py
+++ Midnight/pages/modupdater.py
@@ -16,16 +16,25 @@ def load_deploy():
     return deploy.player_deployments[-10:]
 
 deploys = load_deploy()
-options = {f"{d.file_version} ({d.guid})": d for d in deploys}
-
+sober_cp = "Sober compatible" # SOBER CP 💀💀
+obj = object()
+options = [(sober_cp, obj)] + [
+    (f"{d.file_version} ({d.guid})", d) for d in deploys
+]
 col1, col2 = st.columns(2)
+labels, values = zip(*options)
 
 with col1:
     mod_path = st.text_input("Path to the mod folder", placeholder="e.g : ~/sigmafolder/Blue-star-modz")
 
 with col2:
-    mod_ver_display = st.selectbox("Mod version", options=list(options.keys()))
-    mod_ver = options[mod_ver_display]
+    mod_ver_display = st.selectbox("Mod version", options=labels)
+    selected = values[labels.index(mod_ver_display)]
+
+    if selected is obj:
+        mod_ver = "do sum"
+    else:
+        mod_ver = selected.file_version
 
     if st.button("Update"):
         print("sigma! its do nothing!")
